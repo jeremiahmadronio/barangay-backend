4bc90f44ce21155442506a00063451d6fece8444
jeremiahhhhhhhhhh <jeremiahmadronio2003@gmail.com>
2026-02-27 02:24:56 +0800

    feat: initial set up for barangay management system

---
+++ src/main/java/com/example/barangay/BarangayApplication.java
@@ -0,0 +1,13 @@
+package com.example.barangay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarangayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarangayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/barangay/BarangayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barangay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarangayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
